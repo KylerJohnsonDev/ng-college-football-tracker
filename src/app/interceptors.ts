ef99001ea62cf651c1e519663a26286fa7ee5b7d
Kyler Johnson <kjohnson@octolabs.io>
2022-11-13 15:00:37 -0500

    add functional interceptor and register with new withInterceptors api

---
+++ src/app/interceptors.ts
@@ -0,0 +1,11 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { API_KEY } from 'src/api-key';
+
+export const sportsApiInterceptor: HttpInterceptorFn = (req, next) => {
+  if (req.url.includes('/api.sportsdata.io')) {
+    const url = `${req.url}?key=${API_KEY}`;
+    const newReq = req.clone({ url });
+    return next(newReq);
+  }
+  return next(req);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 import { provideStore } from '@ngrx/store';
+import { sportsApiInterceptor } from './app/interceptors';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([sportsApiInterceptor])),
     importProvidersFrom(BrowserAnimationsModule),
     provideStore(),
   ],
