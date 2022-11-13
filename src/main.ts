328093467a78f173ac7bbebe17cbb0cbc796dcd3
Kyler Johnson <kjohnson@octolabs.io>
2022-11-13 12:15:29 -0500

    configure default theme (dark)

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [provideRouter(routes), provideHttpClient(), importProvidersFrom(BrowserAnimationsModule)],
 }).catch((err) => console.error(err));
